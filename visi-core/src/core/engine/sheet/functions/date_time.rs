0b0c0ffa12baa16cd9cca0fb429dcf94a5a03ced
Albert Yu <albert.yu@gmail.com>
2026-08-20 21:39:28 -0400

    Update TIMEVALUE to accept non-strings, update fuzzer

--- visi-core/src/core/engine/sheet/functions/date_time.rs
+++ visi-core/src/core/engine/sheet/functions/date_time.rs
@@ -114,13 +114,14 @@ impl Sheet {
                 let s = self.to_f64_arg(evaluated_args.get(2), "TIME")?;
                 res_to_rd(crate::core::date_fn::time_fn(h, m, s))
             }
-            "TIMEVALUE" => {
-                let text = evaluated_args
-                    .first()
-                    .map(|v| v.to_string())
-                    .unwrap_or_default();
-                res_to_rd(crate::core::date_fn::timevalue(&text))
-            }
+            "TIMEVALUE" => match evaluated_args.first() {
+                Some(ResultData::Integer(_)) => Ok(ResultData::Float(0.0)),
+                Some(ResultData::Float(f)) => Ok(ResultData::Float(f.fract().abs())),
+                arg => {
+                    let text = arg.map(|v| v.to_string()).unwrap_or_default();
+                    res_to_rd(crate::core::date_fn::timevalue(&text))
+                }
+            },
             "WEEKDAY" => {
                 let s = self.to_f64_arg(evaluated_args.first(), "WEEKDAY")?;
                 let r_type = evaluated_args.get(1).and_then(|v| self.to_f64(v));
